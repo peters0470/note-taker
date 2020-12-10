319c56114793af6bf72d3681655e5063257c5f9b
unknown <peters0470@gmail.com>
2020-12-10 10:14:01 -0600

    added path to index.html when page is called

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+require("./routes/routes_html")(app);
 
 
 app.listen(PORT, () => {
