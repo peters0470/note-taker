82bde33dce3a68d7fb5431ff6bef29274f1dc56e
unknown <peters0470@gmail.com>
2020-12-11 13:02:12 -0600

    added function to grab notes when directed to the notes page

--- routes/routes_api.js
+++ routes/routes_api.js
@@ -1,2 +1,18 @@
 const fs = require('fs');
 const data = require('../db/db.json')
+
+module.exports = (app) => {
+     
+    app.get("/api/notes", (request, result) => {
+        fs.readFile("./db/db.json", "utf-8", (err, notes) => {
+        if (err) throw (err);
+        let notesParsed;
+        try {
+            notesParsed = [].concat(JSON.parse(notes));
+        } catch (err) {
+            notesParsed = [];
+        }
+        result.json(notesParsed);
+    })
+});
+}
